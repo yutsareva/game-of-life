9f326618b5831c948052240a7662533867128c08
Georgii Kolesnikov <kolesnikov.g@tn.ru>
2021-04-09 12:23:48 +0300

    displayDone

--- src/main/java/ru/hse/java/Display.java
+++ src/main/java/ru/hse/java/Display.java
@@ -3,5 +3,16 @@ package ru.hse.java;
 import ru.hse.java.automation.Field;
 
 public interface Display {
-  void display(Field field);
+  default void display(Field field) {
+    for (int i = 0; i < field.getHeight(); i++) {
+      for (int j = 0; j < field.getWidth(); j++) {
+        if (field.isAlive(i, j)) {
+          System.out.print(".");
+        } else {
+          System.out.print("*");
+        }
+        System.out.println();
+      }
+    }
+  };
 }
