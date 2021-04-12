f5d6b5c5c149a2a2a7f4549957d7d6574a0aee6b
Zufar <zufar25@yandex.ru>
2021-04-12 20:36:57 +0300

    added asking user to continue/start from begining/finish the game

--- src/main/java/ru/hse/java/Runner.java
+++ src/main/java/ru/hse/java/Runner.java
@@ -4,6 +4,8 @@ import ru.hse.java.automation.StandartCellularAutomaton;
 import ru.hse.java.settings.Settings;
 import ru.hse.java.settings.SettingsFromFile;
 import ru.hse.java.settings.StandartSettings;
+
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Runner {
@@ -18,9 +20,25 @@ public class Runner {
 
   public void run() throws InterruptedException {
     StandartCellularAutomaton automation = new StandartCellularAutomaton(settings);
-    for (int i = 0; i < settings.getIterationCount() || settings.getIterationCount() < 0; i++) {
-      display.display(automation.getNextIteration());
-      TimeUnit.MILLISECONDS.sleep(settings.getIterationTimeInterval().toMillis());
+    while (true) {
+      for (int i = 0; i < settings.getIterationCount() || settings.getIterationCount() < 0; i++) {
+        display.display(automation.getNextIteration());
+        TimeUnit.MILLISECONDS.sleep(settings.getIterationTimeInterval().toMillis());
+      }
+      Scanner scanner = new Scanner(System.in);
+      while (true) {
+        System.out.println("Do you want to continue(C), start from begin(S) or finish(F)? C/S/F (default=[F])");
+        String input_from_user = scanner.next();
+        System.out.println();
+        if (input_from_user.equalsIgnoreCase("S")) {
+          automation = new StandartCellularAutomaton(settings);
+          break;
+        } else if (input_from_user.equalsIgnoreCase("C")) {
+          break;
+        } else if (input_from_user.equalsIgnoreCase("F")) {
+          return;
+        }
+      }
     }
   }
 }
