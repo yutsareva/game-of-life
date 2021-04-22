d856eec4d23faae73eff5bc660f2fbc5750a35d2
Zufar <zufar25@yandex.ru>
2021-04-22 23:42:22 +0300

    writer done

--- src/main/java/ru/hse/java/writer/FilesWriter.java
+++ src/main/java/ru/hse/java/writer/FilesWriter.java
@@ -1,6 +1,7 @@
 package ru.hse.java.writer;
 
 import org.apache.commons.lang3.tuple.Pair;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
@@ -26,7 +27,7 @@ public class FilesWriter extends Writer {
     }
 
     @Override
-    public void writeSettings(Settings settings) {
+    public void writeSettings(Settings settings) {  //TODO O(n^2) -> O(n)
         writeHeight(settings.getHeight());
         writeWidth(settings.getWidth());
         writeIterationTimeInterval(settings.getIterationTimeInterval());
@@ -96,21 +97,93 @@ public class FilesWriter extends Writer {
 
     @Override
     public void writeAliveCell(Pair<Integer, Integer> aliveCell) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject main = (JSONObject) parser.parse(new FileReader(settingsFileName));
+            JSONObject settings = (JSONObject) main.get("settings");
+            JSONObject initial_configuration = (JSONObject) settings.get("initial_configuration");
+            JSONArray alive_points = (JSONArray) initial_configuration.get("alive_points");
 
+            JSONObject cell = new JSONObject();
+            cell.put("x", aliveCell.getKey());
+            cell.put("y", aliveCell.getValue());
+            alive_points.add(cell);
+
+            FileWriter writer = new FileWriter(settingsFileName);
+            main.writeJSONString(writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException | ParseException e) {
+            System.out.println("in writeIterationTimeInterval catch");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void writeInitialAliveCells(List<Pair<Integer, Integer>> initialAliveCells) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject main = (JSONObject) parser.parse(new FileReader(settingsFileName));
+            JSONObject settings = (JSONObject) main.get("settings");
+            JSONObject initial_configuration = (JSONObject) settings.get("initial_configuration");
+            JSONArray alive_points = (JSONArray) initial_configuration.get("alive_points");
 
+            alive_points.clear();
+
+            for (Pair<Integer, Integer> aliveCell : initialAliveCells) {
+                JSONObject cell_object = new JSONObject();
+                cell_object.put("x", aliveCell.getKey());
+                cell_object.put("y", aliveCell.getValue());
+                alive_points.add(cell_object);
+            }
+
+            FileWriter writer = new FileWriter(settingsFileName);
+            main.writeJSONString(writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException | ParseException e) {
+            System.out.println("in writeIterationTimeInterval catch");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void writeIterationCount(int iterationCount) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject main = (JSONObject) parser.parse(new FileReader(settingsFileName));
+            JSONObject settings = (JSONObject) main.get("settings");
 
+            settings.put("iteration_count", iterationCount);
+
+            FileWriter writer = new FileWriter(settingsFileName);
+            main.writeJSONString(writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException | ParseException e) {
+            System.out.println("in writeIterationTimeInterval catch");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void writeAutomationRules(AutomationRules rules) {
+        JSONParser parser = new JSONParser();
+        try {
+            JSONObject main = (JSONObject) parser.parse(new FileReader(settingsFileName));
+            JSONObject settings = (JSONObject) main.get("settings");
+            JSONObject automation_rules = (JSONObject) settings.get("automation_rules");
 
+            automation_rules.put("alive_neighbors_to_revive", rules.getAliveNeighborsToRevive());
+            automation_rules.put("alive_neighbors_to_die", rules.getAliveNeighborsToDie());
+
+            FileWriter writer = new FileWriter(settingsFileName);
+            main.writeJSONString(writer);
+            writer.flush();
+            writer.close();
+        } catch (IOException | ParseException e) {
+            System.out.println("in writeIterationTimeInterval catch");
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/ru/hse/java/writer/FilesWriterTest.java
+++ src/test/java/ru/hse/java/writer/FilesWriterTest.java
@@ -1,21 +1,77 @@
 package ru.hse.java.writer;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.junit.Test;
+import ru.hse.java.settings.AutomationRules;
 
 import java.time.Duration;
+import java.util.*;
+
+import org.apache.commons.lang3.tuple.Pair;
+import ru.hse.java.settings.Settings;
+import ru.hse.java.settings.parser.SettingsParser;
+
+import static org.junit.Assert.*;
 
 public class FilesWriterTest {
-    public final String path = "src/main/resources/examples/kek.json";
+    public final String path = "src/main/resources/examples/test_configuration.json";
 
     @Test
     public void writeHeightTest() {
+        Settings previous_settings = SettingsParser.parse(path);
+        assert previous_settings != null;
+        Integer previous_height = previous_settings.getHeight();
+
+        Integer new_height = 33;
         FilesWriter curWriter = new FilesWriter(path);
-        curWriter.writeHeight(33);
+        curWriter.writeHeight(new_height);
+
+        Settings current_settings = SettingsParser.parse(path);
+        assert current_settings != null;
+        assertEquals(new_height, current_settings.getHeight());
+
+        curWriter.writeHeight(previous_height);
+        Settings new_settings = SettingsParser.parse(path);
+        assert new_settings != null;
+        assertSame(previous_height, new_settings.getHeight());
     }
 
     @Test
     public void writeIterationTimeInterval() {
         FilesWriter curWriter = new FilesWriter(path);
         curWriter.writeIterationTimeInterval(Duration.ofMillis(250L));
+    }
+
+    @Test
+    public void writeIterationCount() {
+        FilesWriter curWriter = new FilesWriter(path);
+        curWriter.writeIterationCount(20);
+    }
+
+    @Test
+    public void writeAutomationRules() {
+        FilesWriter curWriter = new FilesWriter(path);
+        Set<Integer> alive_neighbors_to_revive = new HashSet<Integer>(Arrays.asList(2, 8));
+        Set<Integer> alive_neighbors_to_die = new HashSet<Integer>(Arrays.asList(0, 4, 5, 6, 7));
+        AutomationRules rules = new AutomationRules(alive_neighbors_to_revive, alive_neighbors_to_die);
+        curWriter.writeAutomationRules(rules);
+    }
+
+    @Test
+    public void writeInitialAliveCells() {
+        FilesWriter curWriter = new FilesWriter(path);
+        List<Pair<Integer, Integer>> initialAliveCells = new ArrayList<>();
+        initialAliveCells.add(new ImmutablePair<Integer, Integer>(1, 1));
+        initialAliveCells.add(new ImmutablePair<Integer, Integer>(2, 2));
+        initialAliveCells.add(new ImmutablePair<Integer, Integer>(3, 3));
+        initialAliveCells.add(new ImmutablePair<Integer, Integer>(4, 4));
+        curWriter.writeInitialAliveCells(initialAliveCells);
+    }
+
+    @Test
+    public void writeAliveCell() {
+        FilesWriter curWriter = new FilesWriter(path);
+        Pair<Integer, Integer> aliveCell = new ImmutablePair<Integer, Integer>(1, 1);
+        curWriter.writeAliveCell(aliveCell);
     }
 }
