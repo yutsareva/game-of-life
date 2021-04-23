97ac9ca5f5d218ce2e0b03210474733024494f5f
Avonazhrets <avonazhrets@gmail.com>
2021-04-23 12:14:36 +0300

    add reset

--- src/main/java/ru/hse/java/commands/ResetCommand.java
+++ src/main/java/ru/hse/java/commands/ResetCommand.java
@@ -1,6 +1,10 @@
 package ru.hse.java.commands;
 
 import com.beust.jcommander.Parameters;
+import ru.hse.java.CLI;
+import ru.hse.java.reader.FilesReader;
+import ru.hse.java.settings.Settings;
+import ru.hse.java.writer.FilesWriter;
 
 @Parameters(
     commandNames = { "reset" },
@@ -10,7 +14,10 @@ public class ResetCommand implements Command {
 
   @Override
   public void run() {
-    System.out.println("ResetCommand");
+    FilesReader reader = new FilesReader(CLI.DEFAULT_CONFIG_FILE);
+    FilesWriter writer = new FilesWriter(CLI.CURRENT_CONFIG_FILE);
+    Settings settings = reader.readSettings();
+    writer.writeSettings(settings);
   }
 
   @Override
