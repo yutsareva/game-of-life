1f1cd27c6ebb6c65341dcfdd8f128dbd217b7190
Avonazhrets <avonazhrets@gmail.com>
2021-04-23 12:32:24 +0300

    add dot command

--- src/main/java/ru/hse/java/commands/AddDotCommand.java
+++ src/main/java/ru/hse/java/commands/AddDotCommand.java
@@ -3,7 +3,12 @@ package ru.hse.java.commands;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
 import java.util.List;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import ru.hse.java.CLI;
+import ru.hse.java.reader.FilesReader;
+import ru.hse.java.settings.Settings;
+import ru.hse.java.writer.FilesWriter;
 
 @Parameters(
     commandNames = { "add_dot" },
@@ -14,30 +19,39 @@ public class AddDotCommand implements Command {
   @Parameter(
       names = "--place",
       description = "Add alive cell by coordinates.",
-      required = true
+      required = true,
+      arity = 2
   )
   private List<Integer> coordinates;
 
+  private Settings settings;
+
   @Override
   public void run() {
-    System.out.println("AddDotCommand");
+    FilesReader reader = new FilesReader(CLI.CURRENT_CONFIG_FILE);
+    settings = reader.readSettings();
+    check_flags();
   }
 
   @Override
   public boolean check_flags() {
-    if (coordinates.size() != 2) {
-      System.out.println("'--place' should be an array of size 2.");
+    if (coordinates.get(0) < 0 || coordinates.get(1) >= settings.getHeight() ||
+        coordinates.get(1) < 0 || coordinates.get(1) >= settings.getWidth()) {
+      System.out.println("Coordinates are out of board limits. Current board size is: " + settings.getHeight() + settings.getWidth());
       return false;
     }
-    // Change after Reader is implemented.
-//    var boardSize = Reader().Read(CLI.CURRENT_PARAMETERS_FILE).getSize();
-//    if (coordinates.get(0) < 0 || coordinates.get(1) >= boardSize.get(0) ||
-//        coordinates.get(1) < 0 || coordinates.get(1) >= boardSize.get(1)) {
-//      System.out.println("Coordinates are out of board limits. Current board size is: ");
-//      return;
-//    }
 
-    // Check if cell is already alive and add if to the board if it's not.
+    List<Pair<Integer, Integer>> alive_cells = settings.getInitialAliveCells();
+    for (Pair<Integer, Integer> alive_cell : alive_cells) {
+      if (alive_cell.getLeft().equals(coordinates.get(0)) &&
+          alive_cell.getRight().equals(coordinates.get(1))) {
+        return true;
+      }
+    }
+
+    FilesWriter writer = new FilesWriter(CLI.CURRENT_CONFIG_FILE);
+    writer.writeAliveCell(
+        new ImmutablePair<Integer, Integer>(coordinates.get(0), coordinates.get(1)));
 
     return true;
   }
