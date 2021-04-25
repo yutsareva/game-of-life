67f7eb1eea33f160c0d7c4f31ff41ed06da672c9
Avonazhrets <avonazhrets@gmail.com>
2021-04-25 22:52:52 +0300

    set_rules command

--- src/main/java/ru/hse/java/commands/SetRulesCommand.java
+++ src/main/java/ru/hse/java/commands/SetRulesCommand.java
@@ -2,6 +2,13 @@ package ru.hse.java.commands;
 
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import java.util.HashSet;
+import java.util.Set;
+import ru.hse.java.CLI;
+import ru.hse.java.reader.FilesReader;
+import ru.hse.java.settings.AutomationRules;
+import ru.hse.java.settings.Settings;
+import ru.hse.java.writer.FilesWriter;
 
 @Parameters(
     commandNames = { "set_rules" },
@@ -10,14 +17,37 @@ import com.beust.jcommander.Parameters;
 public class SetRulesCommand implements Command {
 
   @Parameter(
-      names = "--file",
-      description = "Input file."
+      names = "--revive",
+      description = "Alive neighbors to revive. Format: '1,2,3'"
   )
-  private String file;
+  private String revive;
 
+  @Parameter(
+      names = "--die",
+      description = "Alive neighbors to die. Format: '1,2,3'"
+  )
+  private String die;
   @Override
   public void run() {
-    System.out.println("InitializeCommand");
+    String[] reviveArr = new String[]{};
+    if (revive != null) {
+      reviveArr = revive.split(",");
+    }
+    String[] dieArr = new String[]{};
+    if (die != null) {
+      dieArr = die.split(",");
+    }
+    Set<Integer> reviveSet = new HashSet<Integer>();
+    Set<Integer> dieSet = new HashSet<Integer>();
+    for (var val : reviveArr) {
+      reviveSet.add(Integer.parseInt(val));
+    }
+    for (var val : dieArr) {
+      dieSet.add(Integer.parseInt(val));
+    }
+    FilesWriter writer = new FilesWriter(CLI.CURRENT_CONFIG_FILE);
+    AutomationRules rules = new AutomationRules(reviveSet, dieSet);
+    writer.writeAutomationRules(rules);
   }
 
   @Override
